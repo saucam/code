30185b3c9556f8554ec4e7af5882ffbb8636340e
Yash Datta <ydatta@asu.edu>
2020-09-14 14:05:14 +0800

    CF-71A-SCL: Add solution to way too long words

---
+++ codeforces/71A/waytoolong.scala
@@ -0,0 +1,12 @@
+import scala.io.StdIn.{ readInt, readLine}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val n = readInt
+    (0 until n).foreach { k =>
+      val s = readLine
+      val result = if (s.length  > 10) s"${s.head}${s.length - 2}${s.last}" else s
+      println(result)
+    }
+  }
+}
